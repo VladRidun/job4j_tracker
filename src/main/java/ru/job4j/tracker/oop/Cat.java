2825141ab7ec5e6f482fe975fc6f559e90a12248
VladRidun <ridunvv@gmail.com>
2022-02-01 17:56:38 +0400

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,6 +1,22 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println("Кличка кота " + this.name);
+        System.out.println("Что любит поесть " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
@@ -9,7 +25,17 @@ public class Cat {
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        Cat vaska = new Cat();
+        Cat pushok = new Cat();
+
         String say = peppy.sound();
         System.out.println("peppy says " + say);
+
+        vaska.giveNick("Vaska");
+        vaska.eat("Meet");
+        vaska.show();
+        pushok.giveNick("Pushok");
+        pushok.eat("Wiskas");
+        pushok.show();
     }
 }
