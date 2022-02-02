ea3c0360471f5544fab308651710ab5c2dac21c9
VladRidun <ridunvv@gmail.com>
2022-02-02 09:31:01 +0400

    1.8. Состояние объекта [#167388 #258890]

--- src/test/java/ru/job4j/oop/BatteryTest.java
+++ src/test/java/ru/job4j/oop/BatteryTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.oop;
 
 import org.junit.Test;
-import ru.job4j.tracker.oop.Battery;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
