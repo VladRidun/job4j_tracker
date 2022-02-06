538e723411e0e1edd255a3279ec5c694cda1f1b4
VladRidun <ridunvv@gmail.com>
2022-02-06 12:36:41 +0400

    2. Модель данных. [#174683 #260980]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -3,10 +3,10 @@ package ru.job4j.pojo;
 public class College {
     public static void main(String[] args) {
         Student student = new Student("Ридун", "Владислав", "Валерьевич", 131, "26.07.2009");
-        System.out.println(student.getSurname() + "\n"
-                + student.getName() + "\n"
-                + student.getSecondName() + "\n"
-                + "группа " +  student.getGroup() + "\n"
+        System.out.println(student.getSurname() + System.lineSeparator()
+                + student.getName() + System.lineSeparator()
+                + student.getSecondName() + System.lineSeparator()
+                + "группа " +  student.getGroup() + System.lineSeparator()
                 + student.getReceiptDate());
     }
 }
