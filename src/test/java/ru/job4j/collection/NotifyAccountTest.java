2391efb500291fbc992e2b8e17f6ec9cf4988d5a
VladRidun <ridunvv@gmail.com>
2022-03-04 13:07:16 +0400

    3. Модели данных и HashSet. [#218530 #270256]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -34,9 +34,8 @@ public class NotifyAccountTest {
         );
         HashSet<Account> expect = new HashSet<>(
                 Arrays.asList(
-                        new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("123", "Petr Arsentev", "eDer3432f")
-                )
+                        )
         );
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
