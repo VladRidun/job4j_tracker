a8b67c0ae4a9650f6eaf33fe2adbe0c4f63d3d56
VladRidun <ridunvv@gmail.com>
2022-03-30 13:16:11 +0400

    2. Список адресов [#110062 #282107]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,10 +8,15 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-              return profiles.stream().map(profile -> profile.getAddress()).sorted(new SortByCity()).collect(Collectors.toList());
+              return profiles.stream()
+                      .map(Profile::getAddress)
+                      .sorted(new SortByCity())
+                      .collect(Collectors.toList());
     }
 }
